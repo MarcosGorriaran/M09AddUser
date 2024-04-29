84f1bf8e8b74a6279f7b5d51a29871e615ab52e5
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-29 13:57:18 +0200

    Created model Clients

---
+++ AddUser/Models/Clients.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddUser.Models
+{
+    public class Client
+    {
+        public string DNI { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime SubDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ModDate { get; set; }
+    }
+}
